25d361b828e0d80e6f964175a1eb3a2cc84d446c
mslmyrtd <mslmyrtd@gmail.com>
2022-04-03 22:08:28 +0300

    fetch data with axios

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,19 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const main = () => {
-  return <div>main</div>;
+const Main = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState();
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    const response = await axios(
+      `https://seffaflik.epias.com.tr/transparency/service/market/intra-day-trade-history?endDate=2022-01-26&startDate=2022-01-26`
+    );
+  };
+
+  return <div></div>;
 };
 
-export default main;
+export default Main;
